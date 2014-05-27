3bdceb0a6501cbe736f63eaa5d14aa1966581395
Logan Cai <logan.cai@gmail.com>
2014-05-26 19:05:55 -0700

    Placed an area where the math Calculation should go

--- src/com/cgii/humanblackbox/SensorServices.java
+++ src/com/cgii/humanblackbox/SensorServices.java
@@ -1,5 +1,7 @@
 package com.cgii.humanblackbox;
 
+import java.util.ArrayList;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,22 +13,72 @@ import android.util.Log;
 public class SensorServices extends Services implements SensorEventListener{
 	
 	public static boolean mTracking;
-//	public static boolean mRecording;
+	private ArrayList<SensorEvent> mArrayList;
+	private final int MAX_ARRAY_LENGTH = 45;
 	
 	public SensorServices() {
 		Log.v(Services.TAG, "SensorServices constructor");
 		mTracking = false;
 		Services.isRecording = false;
 	}
+	
+	/*
+	 * This calculates the average/mean of X, Y, Z. 
+	 * Warning: Values from SensorEvent can be positive and negative.
+	 * So in theory of large numbers, X, Y, Z, should = 0 but this is
+	 * not going to be true because the user is usually sitting in 1 position
+	 */
+	
+	private double meanX(){
+		double sum = 0;
+		for(int i = 0; i < mArrayList.size(); i++){
+			sum += mArrayList.get(i).values[0];
+		}
+		return sum/mArrayList.size();
+	}
+	private double meanY(){
+		double sum = 0;
+		for(int i = 0; i < mArrayList.size(); i++){
+			sum += mArrayList.get(i).values[1];
+		}
+		return sum/mArrayList.size();
+	}
+	private double meanZ(){
+		double sum = 0;
+		for(int i = 0; i < mArrayList.size(); i++){
+			sum += mArrayList.get(i).values[2];
+		}
+		return sum/mArrayList.size();
+	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-//		Log.v(Services.TAG, "SensorServices onSensor Change");
 		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
 			if (Services.isRecording == false){
 				//Can write directly to view here with the values
 				Services.mSensorEvent = event;
 				
+				/*
+				 * START: Math calculations goes here.
+				 * To call camera, you must do these 2 commands.
+				 * 1) Services.isRecording = true;
+				 * 2) startRecording();
+				 */
+				
+				if (mArrayList.size() > MAX_ARRAY_LENGTH){
+					mArrayList.remove(0);
+				}
+				else{
+					mArrayList.add(event);
+				}
+				
+				/*
+				 * End: Math calculation goes here.
+				 */
+				
+				/*
+				 * START: Keep this code for demo.
+				 */
 				double vector = Math.sqrt(event.values[0]*event.values[0]+
 						event.values[1]*event.values[1]+
 						event.values[2]*event.values[2]);
@@ -36,6 +88,9 @@ public class SensorServices extends Services implements SensorEventListener{
 					startRecording();
 					
 				}
+				/*
+				 * END: Keep this code for demo.
+				 */
 			}
 		}
 	}
